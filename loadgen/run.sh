2bf31112b447bb7ba7393cf53ddbbe1f015263bf
Jason Schmidt <jay.a.schmidt@gmail.com>
2020-09-03 12:36:32 -0600

    Just post testing

--- loadgen/run.sh
+++ loadgen/run.sh
@@ -8,9 +8,9 @@ jq -ncM 'while(true; .+1) | {method: "POST", url: "https://hamburg-main.tdg.mobi
   tee results.bin | \
   /usr/local/bin/vegeta report
 
-echo "GET TEST: Rate is $THERATE/s DURATION is $THEDURATION"
-jq -ncM 'while(true; .+1) | {method: "GET", url: ("https://hamburg-main.tdg.mobiledgex.net:10001/posts/"+ (.| tostring))}' |
-  /usr/local/bin/vegeta attack -rate=$THERATE/s -lazy -format=json -duration=$THEDURATION | \
-  tee results.bin | \
-  /usr/local/bin/vegeta report
+#echo "GET TEST: Rate is $THERATE/s DURATION is $THEDURATION"
+#jq -ncM 'while(true; .+1) | {method: "GET", url: ("https://hamburg-main.tdg.mobiledgex.net:10001/posts/"+ (.| tostring))}' |
+#  /usr/local/bin/vegeta attack -rate=$THERATE/s -lazy -format=json -duration=$THEDURATION | \
+#  tee results.bin | \
+#  /usr/local/bin/vegeta report
   
